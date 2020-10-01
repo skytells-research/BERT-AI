df2c89493ec84eb5504ef56e2e96fbda148ec244
Skytells, Inc <support@skytells.net>
2020-10-01 22:15:54 +0200

    ..

--- bert.py
+++ bert.py
@@ -1,5 +1,5 @@
 from transformers import pipeline
-
+import gc
 class QA: 
     def __init__(self):
         return self
@@ -8,10 +8,12 @@ class QA:
         question_answerer = pipeline('question-answering')
         out = question_answerer({'question': question,'context': context})
         #print(out)
+        gc.collect()
         return out
 
     def generateText(context, max_length, do_sample=False):
         text_generator = pipeline("text-generation")
         resutls = text_generator(context, max_length=max_length, do_sample=do_sample)
         print(resutls)
+        gc.collect()
         return resutls
